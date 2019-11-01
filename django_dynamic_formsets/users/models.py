7d91361e9386d28ba3621eecda992b721472b01f
Paul Louis <iampaullouis@gmail.com>
2019-11-01 17:38:05 +0530

    Add validation to phone numbers using regex

--- django_dynamic_formsets/users/models.py
+++ django_dynamic_formsets/users/models.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db.models import CharField, Model
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 from django.db import models
+from phone_field import PhoneField
 
 
 class User(AbstractUser):
@@ -26,7 +28,10 @@ class Example2Model(TimeStampedModel):
     name = models.CharField(max_length=1024)
     email = models.EmailField()
     address = models.CharField(max_length=2048)
-    phone_number = models.IntegerField()
+    # https://stackoverflow.com/a/19131360
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                 message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
 
     class Meta:
         verbose_name = "Example Two Form"
@@ -34,4 +39,3 @@ class Example2Model(TimeStampedModel):
 
     def __str__(self):
         return f"{self.name}:{self.email}"
-
