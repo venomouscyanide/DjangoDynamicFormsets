70ffb47b164fa9d3b96121ee236f15b597928ea2
Paul Louis <iampaullouis@gmail.com>
2019-11-01 11:36:03 +0530

    Add footer

--- django_dynamic_formsets/example_files/urls.py
+++ django_dynamic_formsets/example_files/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from django_dynamic_formsets.example_files.views import Example1
 
 urlpatterns = [
-    path('', Example1.as_view(), name='example_1')
+    path('example1/', Example1.as_view(), name='example_1')
 ]
