5830255e8835058b2cc1088d12d47e6e8a961046
Paul Louis <iampaullouis@gmail.com>
2019-11-01 09:13:10 +0530

    Bug fixes

--- django_dynamic_formsets/example_files/views.py
+++ django_dynamic_formsets/example_files/views.py
@@ -7,17 +7,17 @@ from django_dynamic_formsets.example_files.forms import ExampleForm1
 
 
 class Example1(View):
-    EXAMPLE_1_PREFIX = "example_1"
+    EXAMPLE_1_PREFIX = "example_1_formset"
 
     def get(self, request):
         context = self._generate_context()
         return render(request, "examples/example_1.html", context)
 
     def post(self, request):
-        print("There is no backend handled. This project simply aims at showcasing dynamic formsets in Django!")
+        print("Refer to the second example on how to handle backend!")
         return HttpResponse('Success', status=200)
 
     def _generate_context(self):
-        example_1_formset = formset_factory(ExampleForm1, extra=1, can_delete=True)
+        example_1_formset = formset_factory(ExampleForm1, extra=1, can_delete=True)(prefix=self.EXAMPLE_1_PREFIX)
         context = {"example_1_formset": example_1_formset}
         return context
